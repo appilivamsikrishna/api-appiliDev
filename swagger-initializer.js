ba56cc5946f1c695a8b7ad6fbbe52ec40f188487
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2023-06-02 05:48:24 +0530

    much needed changes

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,8 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "api_swagger.yml",
     dom_id: '#swagger-ui',
+    defaultModelsExpandDepth: -1,
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
